e3a4cb8507aac921a2eecebd084877af089f3b9a
GitHub Action <action@github.com>
2021-03-16 16:53:19 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/sirywell/lyricalcommitment/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sirywell.lyricalcommitment.services
+
+import com.github.sirywell.lyricalcommitment.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
